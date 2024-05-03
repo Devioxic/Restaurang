9315980a23f513560f3652c3a4d5e4a0d74ae083
Tilly Persson <devioxic@gmail.com>
2024-05-03 16:16:53 +0200

    backup

--- index.js
+++ index.js
@@ -8,8 +8,10 @@ const avbokaButton = document.getElementById("avboka");
 
 const confirmButton = document.getElementById("confirm");
 const cancelButton = document.getElementById("cancel");
+const bookingAccept = document.getElementById("booking-accept");
 
 const dropdown = document.getElementById("table");
+
 
 const tables = {
     1 : "Bord 1 | 4 Platser",
@@ -31,7 +33,54 @@ const tables = {
 }
 
 let bookedTables = [];
+let availableTables = [];
 let que = [];
+
+function showErrorDialog(message) {
+
+}
+
+function isTableAvailable(seats, isOut) { // Only returns boolean if false, otherwise returns table number
+    console.log(typeof seats);
+    console.log(typeof availableTables[0]);
+    if (isOut) {
+        if (seats <= 4) {
+            for (let i = 1; i <= 6; i++) {
+                if (availableTables.includes(i)) {
+                    return i;
+                }
+            }
+            return false;
+        } else {
+            return false;
+        }
+    } else {
+        if (seats == 6) {
+            if (availableTables.includes(13)) {
+                return 13;
+            } else {
+                return false;
+            }
+        } else if (seats == 4) {
+            for (let i = 7; i <= 10; i++) {
+                if (availableTables.includes(i)) {
+                    return i;
+                }
+            }
+            return false;
+        } else if (seats == 2) {
+            for (let i = 7; i <= 16; i++) {
+                if (i == 13) {
+                    continue
+                }
+                if (availableTables.includes(i)) {
+                    return i;
+                }
+            }
+            return false;
+        }
+    }
+}
 
 function clearChildren(element) {
     while (element.firstChild) {
@@ -87,6 +136,33 @@ confirmButton.addEventListener("click", function(e) {
     hideBookingDiv();
 });
 
+bookingAccept.addEventListener("click", function(e) {
+    const seats = document.getElementById("seats").value;
+    const isOut = document.getElementById("out").checked;
+
+    if (seats === 6 && isOut) {
+        showErrorDialog("Max 4 platser vid uteservering");
+    }
+
+    console.log("Seats: " + seats);
+    console.log("Is out: " + isOut);
+
+    const table = isTableAvailable(seats, isOut);
+
+    if (table) {
+        console.log("Table available: " + table);
+    } else {
+        console.log("Table not available");
+        // Lägg till i kö
+    }
+});
+
 cancelButton.addEventListener("click", function(e) {
     hideBookingDiv();
+});
+
+window.addEventListener("load", function(e) {
+    for (let table in tables) {
+        availableTables.push(table);
+    }
 });
